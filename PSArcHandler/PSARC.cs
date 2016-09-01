9f85403996a12af69369dce89b2a9c26fdec4f34
Robert Colbert <rmcolbert@outlook.com>
2016-08-31 23:12:22 -0500

    Fixed issue w/ unpacking files that shared the same blocks.

--- PSArcHandler/PSARC.cs
+++ PSArcHandler/PSARC.cs
@@ -14,13 +14,13 @@ namespace PSArcHandler
 
         private struct Header
         {
-            public uint MagicNumber; // 0x50534152 'PSAR'
-            public uint VersionNumber;
-            public uint CompressionMethod; // zlib or lzma
-            public uint TotalTOCSize; // Includes Header
-            public uint TOCEntrySize; // Size of a single entry in the TOC, currently 30. This allows the size to be expanded in the future while maintaining backward compat.
-            public uint numFiles;
-            public uint blockSize; // The size of each block decompressed.
+            public uint magicNumber;        // 0x50534152 'PSAR'
+            public uint versionNumber;
+            public uint compressionMethod;  // zlib or lzma
+            public uint tocLength;          // Includes Header
+            public uint tocEntrySize;       // Size of a single entry in the TOC, currently 30. This allows the size to be expanded in the future while maintaining backward compat.
+            public uint tocEntries;
+            public uint blockSize;          // The size of each block decompressed.
             public uint archiveFlags;
         }
         private Header psHeader = new Header();
@@ -31,6 +31,8 @@ namespace PSArcHandler
             public uint blockListStart;
             public ulong originalSize;
             public ulong startOffset; // BACK THE READING POINTER UP BY 3!!
+            public ulong actualOffset;
+            public ulong actualBlockStart;
             public string fileName;
 
             public TOCEntry(byte[] MD5, uint blockListStart, ulong originalSize) : this()
@@ -75,12 +77,12 @@ namespace PSArcHandler
         {
             br.BaseStream.Position = 0;  // Rewind the stream just in case
             psHeader = new Header();
-            psHeader.MagicNumber = br.ReadUInt32();
-            psHeader.VersionNumber = br.ReadUInt32();
-            psHeader.CompressionMethod = br.ReadUInt32();
-            psHeader.TotalTOCSize = br.ReadUInt32();
-            psHeader.TOCEntrySize = br.ReadUInt32();
-            psHeader.numFiles = br.ReadUInt32();
+            psHeader.magicNumber = br.ReadUInt32();
+            psHeader.versionNumber = br.ReadUInt32();
+            psHeader.compressionMethod = br.ReadUInt32();
+            psHeader.tocLength = br.ReadUInt32();
+            psHeader.tocEntrySize = br.ReadUInt32();
+            psHeader.tocEntries = br.ReadUInt32();
             psHeader.blockSize = br.ReadUInt32();
             psHeader.archiveFlags = br.ReadUInt32();
         }
@@ -89,15 +91,26 @@ namespace PSArcHandler
         {
             uint i = 0;
             TOC = new List<TOCEntry>();
-            for (i = 0; i < psHeader.numFiles; i++)
+
+            ulong actualOffset = 0;
+            ulong actualBlockStart = 0;
+
+
+            for (i = 0; i < psHeader.tocEntries; i++)
             {
                 TOCEntry tmp = new TOCEntry();
                 tmp.MD5 = br.ReadBytes(16);
                 tmp.blockListStart = br.ReadUInt32();
+
                 tmp.originalSize = FortyBitInt(br.ReadUInt64());
                 br.BaseStream.Position -= 3;
+
                 tmp.startOffset = FortyBitInt(br.ReadUInt64());
                 br.BaseStream.Position -= 3;
+
+                actualOffset += tmp.startOffset;
+                tmp.actualOffset = actualOffset;
+
                 TOC.Add(tmp);
             }
 
@@ -105,8 +118,8 @@ namespace PSArcHandler
             uint cBlockSize = psHeader.blockSize;
 
             // Extract the Manifest File
-            br.BaseStream.Position = (long)TOC[0].startOffset; // Manifest File
-            byte[] DecompressedStream = zlib_net.Inflate(br.ReadBytes((int)(TOC[1].startOffset - TOC[0].startOffset)), zBlocks, cBlockSize, TOC[0].originalSize);
+            br.BaseStream.Position = (long)TOC[0].actualOffset; // Manifest File
+            byte[] DecompressedStream = zlib_net.Inflate(br.ReadBytes((int)(TOC[1].actualOffset - TOC[0].actualOffset)), zBlocks, cBlockSize, TOC[0].originalSize);
 
             TOCList = new List<string>(System.Text.Encoding.Default.GetString(DecompressedStream).Split('\n'));
 
@@ -139,19 +152,22 @@ namespace PSArcHandler
         public UnpackedFile readFile(int manifestLocation)
         {
             byte[] outFile = null;
-            br.BaseStream.Position = (long)TOC[manifestLocation].startOffset; // Manifest File
-            UInt16 isGzipped = br.ReadUInt16();
+            br.BaseStream.Position = (long)TOC[manifestLocation].startOffset; // From manifest
+
+            UInt16 isZipped = br.ReadUInt16();
             br.BaseStream.Position -= 2;    // Rewind 2 bytes
 
-            uint zBlocks = TOC[manifestLocation + 1].blockListStart - TOC[manifestLocation].blockListStart;
-            uint cBlockSize = psHeader.blockSize;
+            ulong cBlockSize = psHeader.blockSize;
+            ulong zBlocks = (TOC[manifestLocation].originalSize - (TOC[manifestLocation].originalSize % cBlockSize)) / cBlockSize; // Calculate # of whole blocks
+            if ((TOC[manifestLocation].originalSize % cBlockSize) > 0) zBlocks++; // Add 1 block if there was leftover data
 
-            ulong fileSize = TOC[manifestLocation + 1].startOffset - TOC[manifestLocation].startOffset;
-
-            if (isGzipped == 0x78da || isGzipped == 0x7801)
-                outFile = zlib_net.Inflate(br.ReadBytes((int)fileSize), zBlocks, cBlockSize, TOC[manifestLocation].originalSize);
+            if (isZipped == 0x78da || isZipped == 0x7801) // Stream is compressed
+            {
+                ulong fileSize = zBlocks * cBlockSize;  // Only pass a part of the whole archive stream to be inflated.
+                outFile = zlib_net.Inflate(br.ReadBytes((int)fileSize), (uint)zBlocks, (uint)cBlockSize, TOC[manifestLocation].originalSize);
+            } 
             else
-                outFile = br.ReadBytes((int)fileSize);
+                outFile = br.ReadBytes((int)TOC[manifestLocation].originalSize);
 
             if (TOC[manifestLocation].originalSize != (ulong)outFile.LongLength)
             {
